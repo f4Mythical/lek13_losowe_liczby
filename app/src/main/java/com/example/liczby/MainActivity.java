1f0a0e348f502b226a39543597427b34030ab529
f4mythical <grynka.adam@gmail.com>
2026-04-14 08:05:07 +0200

    wlaczenie buttonow i wyglad

--- app/src/main/java/com/example/liczby/MainActivity.java
+++ app/src/main/java/com/example/liczby/MainActivity.java
@@ -1,12 +1,16 @@
 package com.example.liczby;
 
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.TextView;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
+
+import java.util.Random;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -20,5 +24,12 @@ public class MainActivity extends AppCompatActivity {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
         });
+        TextView tvRandom = findViewById(R.id.tvRandom);
+        Button btnRand1 = findViewById(R.id.btnRandom1);
+        Button btnRand2 = findViewById(R.id.btnRandom2);
+        Button btnRand3 = findViewById(R.id.btnRandom3);
+        Button btnRand4 = findViewById(R.id.btnRandom4);
+        Button btnRand5 = findViewById(R.id.btnRandom5);
+        Random random = new Random();
     }
 }
